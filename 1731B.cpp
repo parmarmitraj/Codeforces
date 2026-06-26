7263380ca1cbfc6c8ac004ec7918db84cc95ec1f
parmarmitraj <parmarmitraj142@gmail.com>
2026-06-26 17:10:13 +0530

    1731B solved

---
+++ 1731B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1e9 + 7;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        long long ans = ((((n * (n + 1)) % mod) * (4 * n - 1)) % mod * 337) % mod;
+
+    	cout << ans << endl;
+    }
+}
