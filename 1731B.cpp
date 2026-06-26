8a31a46cd2dd9e5f1e5a94e69bb510349c51e680
parmarmitraj <parmarmitraj142@gmail.com>
2026-06-26 17:15:00 +0530

    1731B solved

--- 1731B.cpp
+++ 1731B.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int mod = 1e9 + 7;
+const long long MOD = 1e9 + 7;
+const long long INV6 = 166666668;
 
 int main(){
     int t;
     cin >> t;
     while(t--){
-        int n;
+        long long n;
         cin >> n;
 
-        long long ans = ((((n * (n + 1)) % mod) * (4 * n - 1)) % mod * 337) % mod;
+        long long ans = n % MOD;
+        ans = ans * ((n + 1) % MOD) % MOD;
+        ans = ans * ((4 * n - 1) % MOD) % MOD;
+        ans = ans * INV6 % MOD;
+        ans = ans * 2022 % MOD;
 
-    	cout << ans << endl;
+        cout << ans << '\n';
     }
 }
