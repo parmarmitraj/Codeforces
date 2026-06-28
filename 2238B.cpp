54aad8eba098ccfe8cfe82b350aaffa5611380fe
parmarmitraj <parmarmitraj142@gmail.com>
2026-06-28 20:36:11 +0530

    2238B solved

---
+++ 2238B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        long long ans = 0;
+
+        for (int b = 1; b <= n; b++) {
+            long long multiples = n / b;
+            ans += multiples * multiples; 
+        }
+        
+        cout << ans << endl;
+    }
+}
